bc69131e66466397f0803f0f39f7c95a9042d1b7
Dan Davison <dan.davison@temporal.io>
2025-06-01 21:28:15 -0400

    WIP: test operation handler decorator

---
+++ tests/test_handler_operation_handler_decorators.py
@@ -0,0 +1,35 @@
+from typing import Any, Type
+
+import pytest
+
+import nexusrpc
+
+
+class _TestCase:
+    UserServiceHandler: Type[Any]
+    expected_error_message: str
+
+
+class DuplicateOperationName(_TestCase):
+    class UserServiceHandler:
+        @nexusrpc.handler.operation_handler(name="a")
+        def op_1(self) -> nexusrpc.handler.OperationHandler[int, int]: ...
+
+        # TODO(dan): pyright false positive here
+        @nexusrpc.handler.sync_operation_handler(name="a")
+        def op_2(
+            self, ctx: nexusrpc.handler.StartOperationContext, input: str
+        ) -> int: ...
+
+    expected_error_message = "Operation 'a' has already been defined."
+
+
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        DuplicateOperationName,
+    ],
+)
+def test_service_handler_decorator(test_case: _TestCase):
+    with pytest.raises(RuntimeError, match=test_case.expected_error_message):
+        nexusrpc.handler.service_handler(test_case.UserServiceHandler)
