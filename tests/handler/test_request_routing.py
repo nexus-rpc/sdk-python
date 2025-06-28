b8fe8f6b62f7eb3a3f0853f78a565ffad12df363
Dan Davison <dan.davison@temporal.io>
2025-06-28 18:44:03 -0400

    Test request routing without service definition

--- tests/handler/test_request_routing.py
+++ tests/handler/test_request_routing.py
@@ -18,7 +18,6 @@ from ..helpers import DummySerializer
 
 class _TestCase:
     UserService: Type[Any]
-    # `expected` is (request, handler), where both request and handler are
     # (service_name, op_name)
     supported_request: tuple[str, str]
 
@@ -47,7 +46,7 @@ class NoOverrides(_TestCase):
 
 
 class OverrideServiceName(_TestCase):
-    @nexusrpc.service(name="UserServiceRenamed")
+    @nexusrpc.service(name="UserService-renamed")
     class UserService:
         op: nexusrpc.Operation[None, bool]
 
@@ -57,7 +56,7 @@ class OverrideServiceName(_TestCase):
         async def op(self, ctx: StartOperationContext, input: None) -> bool:
             return await super().op(ctx, input)
 
-    supported_request = ("UserServiceRenamed", "op")
+    supported_request = ("UserService-renamed", "op")
 
 
 class OverrideOperationName(_TestCase):
@@ -74,17 +73,33 @@ class OverrideOperationName(_TestCase):
     supported_request = ("UserService", "op-renamed")
 
 
+class OverrideServiceAndOperationName(_TestCase):
+    @nexusrpc.service(name="UserService-renamed")
+    class UserService:
+        op: nexusrpc.Operation[None, bool] = nexusrpc.Operation(name="op-renamed")
+
+    @service_handler(service=UserService)
+    class UserServiceHandler(_TestCase.UserServiceHandler):
+        @sync_operation
+        async def op(self, ctx: StartOperationContext, input: None) -> bool:
+            return await super().op(ctx, input)
+
+    supported_request = ("UserService-renamed", "op-renamed")
+
+
 @pytest.mark.parametrize(
     "test_case",
     [
         NoOverrides,
         OverrideServiceName,
         OverrideOperationName,
+        OverrideServiceAndOperationName,
     ],
 )
 @pytest.mark.asyncio
-async def test_request_routing_with_service_definition(test_case: _TestCase):
-    handler = Handler(user_service_handlers=[test_case.UserServiceHandler()])
+async def test_request_routing_with_service_definition(
+    test_case: _TestCase,
+):
     request_service, request_op = test_case.supported_request
     ctx = StartOperationContext(
         service=request_service,
@@ -92,6 +107,74 @@ async def test_request_routing_with_service_definition(test_case: _TestCase):
         headers={},
         request_id="request-id",
     )
+    handler = Handler(user_service_handlers=[test_case.UserServiceHandler()])
+    result = await handler.start_operation(
+        ctx, LazyValue(serializer=DummySerializer(None), headers={})
+    )
+    assert cast(StartOperationResultSync[bool], result).value is True
+
+
+class NoServiceDefinitionNoOverrides(_TestCase):
+    @service_handler
+    class UserServiceHandler(_TestCase.UserServiceHandler):
+        @sync_operation
+        async def op(self, ctx: StartOperationContext, input: None) -> bool:
+            return await super().op(ctx, input)
+
+    supported_request = ("UserServiceHandler", "op")
+
+
+class NoServiceDefinitionOverrideServiceName(_TestCase):
+    @service_handler(name="UserServiceHandler-renamed")
+    class UserServiceHandler(_TestCase.UserServiceHandler):
+        @sync_operation
+        async def op(self, ctx: StartOperationContext, input: None) -> bool:
+            return await super().op(ctx, input)
+
+    supported_request = ("UserServiceHandler-renamed", "op")
+
+
+class NoServiceDefinitionOverrideOperationName(_TestCase):
+    @service_handler
+    class UserServiceHandler(_TestCase.UserServiceHandler):
+        @sync_operation(name="op-renamed")
+        async def op(self, ctx: StartOperationContext, input: None) -> bool:
+            return await super().op(ctx, input)
+
+    supported_request = ("UserServiceHandler", "op-renamed")
+
+
+class NoServiceDefinitionOverrideServiceAndOperationName(_TestCase):
+    @service_handler(name="UserServiceHandler-renamed")
+    class UserServiceHandler(_TestCase.UserServiceHandler):
+        @sync_operation(name="op-renamed")
+        async def op(self, ctx: StartOperationContext, input: None) -> bool:
+            return await super().op(ctx, input)
+
+    supported_request = ("UserServiceHandler-renamed", "op-renamed")
+
+
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        NoServiceDefinitionNoOverrides,
+        NoServiceDefinitionOverrideServiceName,
+        NoServiceDefinitionOverrideOperationName,
+        NoServiceDefinitionOverrideServiceAndOperationName,
+    ],
+)
+@pytest.mark.asyncio
+async def test_request_routing_without_service_definition(
+    test_case: _TestCase,
+):
+    request_service, request_op = test_case.supported_request
+    ctx = StartOperationContext(
+        service=request_service,
+        operation=request_op,
+        headers={},
+        request_id="request-id",
+    )
+    handler = Handler(user_service_handlers=[test_case.UserServiceHandler()])
     result = await handler.start_operation(
         ctx, LazyValue(serializer=DummySerializer(None), headers={})
     )
