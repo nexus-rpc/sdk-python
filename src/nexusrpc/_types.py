25af294948acf8d99ddf11d6917d13d49e6a8830
Dan Davison <dan.davison@temporal.io>
2025-06-29 17:19:46 -0400

    Docstrings

--- src/nexusrpc/_types.py
+++ src/nexusrpc/_types.py
@@ -2,14 +2,14 @@ from __future__ import annotations
 
 from typing import TypeVar
 
-# Operation input type
 InputT = TypeVar("InputT", contravariant=True)
+"""Operation input type"""
 
-# Operation output type
 OutputT = TypeVar("OutputT", covariant=True)
+"""Operation output type"""
 
-# A user's service handler class, typically decorated with @service_handler
 ServiceHandlerT = TypeVar("ServiceHandlerT")
+"""A user's service handler class, typically decorated with @service_handler"""
 
-# A user's service definition class, typically decorated with @service
 ServiceDefinitionT = TypeVar("ServiceDefinitionT")
+"""A user's service definition class, typically decorated with @service"""
