5fa431adcb40d4404f2058856cf342fe18ff9636
Dan Davison <dan.davison@temporal.io>
2025-06-30 16:40:45 -0400

    Remove accidentally exposed utility function

--- src/nexusrpc/handler/__init__.py
+++ src/nexusrpc/handler/__init__.py
@@ -7,8 +7,6 @@ responsible for dispatching requests to Nexus operations.
 Nexus service/operation authors will use this module to implement operation handler
 methods within service handler classes.
 """
-# TODO(preview): pass pyright strict mode "python.analysis.typeCheckingMode": "strict"
-# TODO(preview): pass mypy
 
 from __future__ import annotations
 
@@ -24,13 +22,11 @@ from ._common import (
 from ._core import Handler as Handler
 from ._decorators import service_handler, sync_operation
 from ._operation_handler import OperationHandler as OperationHandler
-from ._util import get_start_method_input_and_output_type_annotations
 
 __all__ = [
     "CancelOperationContext",
     "FetchOperationInfoContext",
     "FetchOperationResultContext",
-    "get_start_method_input_and_output_type_annotations",
     "Handler",
     "OperationContext",
     "OperationHandler",
